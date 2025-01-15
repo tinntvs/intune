ac5042476311100b2b0130ac325a7e759dad632c
tinntvs <87159604+tinntvs@users.noreply.github.com>
2025-01-15 13:47:07 +0700

    Update enrollment.sh
    Updated 3 functions
    Updfated repo
    Updated sourcelist.

--- enrollment.sh
+++ enrollment.sh
@@ -7,33 +7,50 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-verx=$(grep -oP '(?<=VERSION_ID=").*(?=")' /etc/os-release)
+function app_msedge()
+{
+  logger -t $logname Install Microsoft Edge Latest version
+  wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_131.0.2903.112-1_amd64.deb
+  sudo apt install -y ./microsoft-edge-stable_131.0.2903.112-1_amd64.deb
+}
+
+function app_msintune()
+{
+  logger -t $logname Install Microsoft Intune Portal
+  if [ -f "/etc/apt/sources.list.d/microsoft-prod.list" ]; then
+    rm -rf /etc/apt/sources.list.d/microsoft-prod.list
+  fi
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+  sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
+  rm microsoft.gpg
+  sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/ubuntu/$(lsb_release -rs)/prod $(lsb_release -cs) main" >> /etc/apt/sources.list.d/microsoft-ubuntu-$(lsb_release -cs)-prod.list'
+  sudo apt update -y
+  sudo apt install intune-portal -y
+}
+function app_mdatp()
+{
+  logger -t $logname Install Microsoft Defender for Endpoint
+  #  wget https://packages.microsoft.com/ubuntu/$verx/prod/pool/main/m/mde-netfilter/mde-netfilter_100.69.73_amd64.deb
+  #  apt install -y ./mde-netfilter_100.69.73_amd64.deb
+  #  apt install -y mdatp
+  curl -o microsoft.list https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list
+  sudo mv ./microsoft.list /etc/apt/sources.list.d/microsoft-prod.list
+  sudo apt-get update -y
+  sudo apt-get install mdatp -y
+}
+
+#main_script
+logger -t $logname - Set the error status
+set -e
+
+logger -t $logname Install pre-requisite packages
+apt install -y curl gpg gnupg wget libplist-utils apt-transport-https software-properties-common
 
 (
-    logger -t $logname - Set the error status
-    set -e
-
-    logger -t $logname Install pre-requisite packages
-    apt install -y curl gpg wget apt-transport-https software-properties-common
-
-    logger -t $logname Install Microsoft Edge Latest version
-    wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_131.0.2903.112-1_amd64.deb
-    sudo apt install -y ./microsoft-edge-stable_131.0.2903.112-1_amd64.deb
-
-    logger -t $logname Install Microsoft Intune app
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-    sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/ 
-    sudo sh -c "echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/ubuntu/$verx/prod jammy main\" > /etc/apt/sources.list.d/microsoft-ubuntu-jammy-prod.list"
-    sudo rm microsoft.gpg
-    apt update
-    apt install -y intune-portal
-
-    logger -t $logname Install Microsoft Defender for Endpoint
-    wget https://packages.microsoft.com/ubuntu/$verx/prod/pool/main/m/mde-netfilter/mde-netfilter_100.69.73_amd64.deb
-    apt install -y ./mde-netfilter_100.69.73_amd64.deb
-    apt install -y mdatp
+  app_msedge
+  app_mdatp
+  app_msintune
 )
-
 ERROR_CODE=$?
 if [ $ERROR_CODE -ne 0 ]; then
     logger -t $logname - There was an error. Please restart the script or contact your admin if the error persists. - $ERROR_CODE
